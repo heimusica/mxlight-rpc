634a02578820fe77dbe67585940105e4232ba305
Matias Insaurralde <matias@insaurral.de>
2014-10-12 05:10:42 -0300

    sample daemon

---
+++ daemon.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require './mxlight-rpc'
+
+mxlightd = MXLight::Daemon.new( :mxlight_root => 'C:\MXLight-2.1.8-demo\',
+                                :default_stream_profile => :RTMP,
+                                :gui_stats => :hide,
+                                :gui_config => :hide )
+
+mxlightd.start()
