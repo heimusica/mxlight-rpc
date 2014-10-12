f75c92111e14da738372602990003ad06d707cdc
Matias Insaurralde <matias@insaurral.de>
2014-10-12 05:10:49 -0300

    structure

--- mxlight-rpc.rb
+++ mxlight-rpc.rb
@@ -1,1 +1,21 @@
+require 'sinatra/base'
+require 'yaml'
 
+module MXLight
+  class RPC < Sinatra::Base
+    get '/' do
+      ''
+    end
+
+    get '/settings'  do
+    end
+
+    post '/settings' do
+    end
+
+  end
+  class Daemon
+    def initialize()
+    end
+  end
+end
